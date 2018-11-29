845b22025b53dcf5e866440285e184b7c3c2fff1
hetpatel33 <patel.het33@gmail.com>
2018-11-29 18:51:49 +0530

    Updates

---
+++ precache-manifest.fe0ac74ecb7fb5e8a428916d1c06cf6f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "34f380cf056f8dc35521",
+    "url": "/TodoMVC/static/css/main.91c49dbe.chunk.css"
+  },
+  {
+    "revision": "34f380cf056f8dc35521",
+    "url": "/TodoMVC/static/js/main.34f380cf.chunk.js"
+  },
+  {
+    "revision": "40524fc8a42ad13d9450",
+    "url": "/TodoMVC/static/js/1.40524fc8.chunk.js"
+  },
+  {
+    "revision": "82fee1271ca2e508fa36",
+    "url": "/TodoMVC/static/js/runtime~main.82fee127.js"
+  },
+  {
+    "revision": "b9746d95bfa64fb144787e020eeed36f",
+    "url": "/TodoMVC/index.html"
+  }
+];
